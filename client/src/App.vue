8c367ad1c5160bd8bb0b07ba1460af560c7439ed
Artem <kroxakawai@gmail.com>
2021-06-03 12:23:23 +0300

    Created ToDo Application

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <section class="todoapp">
+      <PageTitle title="todos app" />
+      <Todo />
+    </section>
+  </div>
+</template>
+
+<script>
+import Todo from "./components/Todo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todo
+  }
+};
+</script>
+
